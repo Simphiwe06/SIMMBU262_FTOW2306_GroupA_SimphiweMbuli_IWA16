33073bb54a3e288c474f740964d04e55d4cbf227
Simphiwe06 <simphiwembuli06@gmail.com>
2023-08-28 00:32:46 +0200

    used correct syntax,properly accessed data, fixed calculations and corrected string concetenation

---
+++ scripts.js
@@ -0,0 +1,113 @@
+const MONTHS = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  
+  const data = {
+    response: {
+      requestType: "FETCH_ATHLETE_DATA",
+      requestBy: "ALL_MATCHING_ATHLETES",
+      forDisplay: "BEST_RACES",
+  
+      data: {
+        NM372: {
+          firstName: "Nwabisa",
+          surname: "Masiko",
+          id: "NM372",
+          races: [
+            {
+              date: '2022-11-18T20:00:00.000Z',
+              time: [9, 7, 8, 6],
+            },
+            {
+              date: '2022-12-02T20:00:00.000Z',
+              time: [6, 7, 8, 7],
+            },
+          ],
+        },
+  
+        SV782: {
+          firstName: "Schalk",
+          surname: "Venter",
+          id: "SV782",
+          races: [
+            {
+              date: '2022-11-18T20:00:00.000Z',
+              time: [10, 8, 3, 12],
+            },
+            {
+              date: '2022-11-25T20:00:00.000Z',
+              time: [6, 8, 9, 11],
+            },
+            {
+              date: '2022-12-02T20:00:00.000Z',
+              time: [10, 11, 4, 8],
+            },
+            {
+              date: '2022-12-09T20:00:00.000Z',
+              time: [9, 8, 9, 11],
+            },
+          ],
+        },
+      },
+    },
+  };
+  
+  // Only edit below this comment
+  
+  const createHtml = (athleteId) => {
+    const athlete = data.response.data[athleteId];
+    const latestRace = athlete.races[athlete.races.length - 1];
+    const [latestTime] = latestRace.time;
+  
+    const fragment = document.createDocumentFragment();
+  
+    const title = document.createElement('h2');
+    title.textContent = athlete.id;
+    fragment.appendChild(title);
+  
+    const list = document.createElement('dl');
+  
+    const date = new Date(latestRace.date);
+    const day = date.getDate();
+    const month = MONTHS[date.getMonth()];
+    const year = date.getFullYear();
+  
+    const hours = Math.floor(latestTime / 60);
+    const minutes = latestTime % 60;
+  
+    list.innerHTML = /* html */ `
+      <dt>Athlete</dt>
+      <dd>${athlete.firstName} ${athlete.surname}</dd>
+  
+      <dt>Total Races</dt>
+      <dd>${athlete.races.length}</dd>
+  
+      <dt>Event Date (Latest)</dt>
+      <dd>${day} ${month} ${year}</dd>
+  
+      <dt>Total Time (Latest)</dt>
+      <dd>${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}</dd>
+    `;
+  
+    fragment.appendChild(list);
+  
+    return fragment;
+  };
+  
+  const NM372 = 'NM372';
+  const SV782 = 'SV782';
+  
+  document.querySelector(`[data-athlete="${NM372}"]`).appendChild(createHtml(NM372));
+  document.querySelector(`[data-athlete="${SV782}"]`).appendChild(createHtml(SV782));
+  
